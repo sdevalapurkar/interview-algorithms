95031f9fc0375d9732f92ea9aaaf58067c621249
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-07 22:32:42 -0800

    add solution and some tests

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,7 +1,19 @@
 // write a function that returns whether or not a given string is a palindrome
 
 function isPalindrome(str) {
+  // we can reverse the given string and then compare it to the original string
+  // if they are equivalent, then the input string is a palindrome
+  let reversedString = '';
 
+  for (let x = str.length - 1; x >= 0; x -= 1) {
+    reversedString += str[x];
+  }
+
+  if (reversedString === str) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = isPalindrome;
--- palindrome/palindrome.test.js
+++ palindrome/palindrome.test.js
@@ -1,5 +1,13 @@
 const isPalindrome = require('./palindrome');
 
-test('returns true if the string is a palindrome', () => {
+test('returns true if the input string is a palindrome', () => {
   expect(isPalindrome('racecar')).toBe(true);
+});
+
+test('returns true if the input string is empty', () => {
+  expect(isPalindrome('')).toBe(true);
+});
+
+test('returns false if the input string is not a palindrome', () => {
+  expect(isPalindrome('abc')).toBe(false);
 });
