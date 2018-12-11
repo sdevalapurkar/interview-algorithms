4bf2ae5996ae1daeb98ed977b4ff8f0a17442560
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-10 21:36:02 -0800

    add fizz buzz

--- fizz-buzz/fizz_buzz.js
+++ fizz-buzz/fizz_buzz.js
@@ -4,7 +4,21 @@
 // for multiples of both 3 and 5, output fizzbuzz
 
 function fizzBuzz(n) {
-  
+  const fizzBuzzArray = [];
+
+  for (let x = 1; x <= n; x += 1) {
+    if (x % 3 === 0 && x % 5 === 0) {
+      fizzBuzzArray.push('fizzbuzz');
+    } else if (x % 3 === 0) {
+      fizzBuzzArray.push('fizz');
+    } else if (x % 5 === 0) {
+      fizzBuzzArray.push('buzz');
+    } else {
+      fizzBuzzArray.push(x);
+    }
+  }
+
+  return fizzBuzzArray;
 }
 
 module.exports = fizzBuzz;
--- fizz-buzz/fizz_buzz.test.js
+++ fizz-buzz/fizz_buzz.test.js
@@ -5,9 +5,9 @@ test('fizzBuzz function is defined', () => {
 });
 
 test('outputs the proper fizz buzz combination of numbers from 1 to 16', () => {
-  const output = fizzBuzz(9);
+  const output = fizzBuzz(16);
 
-  expect(output).toBe([
-    1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz',
-  ]);
+  expect(output).toEqual(expect.arrayContaining([
+    1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16,
+  ]));
 });
