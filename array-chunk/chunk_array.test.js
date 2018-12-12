a17f52751412179d70ed7acf7e9659853c368f97
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-11 20:52:30 -0800

    more test

--- array-chunk/chunk_array.test.js
+++ array-chunk/chunk_array.test.js
@@ -23,3 +23,11 @@ test('chunks a given array as expected', () => {
 test('chunks an empty input array with any given size', () => {
   expect(chunk([], 8)).toEqual(expect.arrayContaining([]));
 });
+
+test('chunks a given array as expected where size > array length', () => {
+  const chunkedArray = chunk([1, 2, 3, 4, 5, 6, 7, 8], 10);
+
+  expect(chunkedArray).toEqual(expect.arrayContaining([
+    [1, 2, 3, 4, 5, 6, 7, 8],
+  ]));
+});
