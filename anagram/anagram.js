fb97b2855495820d498bc443dda531c26bcaa079
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-15 22:30:41 -0800

    add initial solution to anagram

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -3,7 +3,37 @@
 // in the same quantity
 
 function isAnagram(stringA, stringB) {
+  const strippedStringA = stringA.replace(/[^A-Z0-9]+/ig, '').toLowerCase();
+  const strippedStringB = stringB.replace(/[^A-Z0-9]+/ig, '').toLowerCase();
+  const charMapA = {};
+  const charMapB = {};
 
+  for (let x = 0; x < strippedStringA.length; x += 1) {
+    if (charMapA[strippedStringA[x]] >= 1) {
+      charMapA[strippedStringA[x]] += 1;
+    } else {
+      charMapA[strippedStringA[x]] = 1;
+    }
+  }
+
+  for (let x = 0; x < strippedStringB.length; x += 1) {
+    if (charMapB[strippedStringB[x]] >= 1) {
+      charMapB[strippedStringB[x]] += 1;
+    } else {
+      charMapB[strippedStringB[x]] = 1;
+    }
+  }
+
+  const aProps = Object.getOwnPropertyNames(charMapA);
+  const bProps = Object.getOwnPropertyNames(charMapB);
+
+  if (aProps.length !== bProps.length) return false;
+
+  for (let x = 0; x < aProps.length; x += 1) {
+    if (charMapA[x] !== charMapB[x]) return false;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
