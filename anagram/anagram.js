16c8fc41833402b74d2b3f25d7f03d95d5a5df1c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-15 22:36:02 -0800

    refactor

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -2,6 +2,19 @@
 // a string is an anagram of another string if it uses the same characters
 // in the same quantity
 
+function areEqualObjects(objA, objB) {
+  const aProps = Object.getOwnPropertyNames(objA);
+  const bProps = Object.getOwnPropertyNames(objB);
+
+  if (aProps.length !== bProps.length) return false;
+
+  for (let x = 0; x < aProps.length; x += 1) {
+    if (objA[x] !== objB[x]) return false;
+  }
+
+  return true;
+}
+
 function isAnagram(stringA, stringB) {
   const strippedStringA = stringA.replace(/[^A-Z0-9]+/ig, '').toLowerCase();
   const strippedStringB = stringB.replace(/[^A-Z0-9]+/ig, '').toLowerCase();
@@ -24,16 +37,7 @@ function isAnagram(stringA, stringB) {
     }
   }
 
-  const aProps = Object.getOwnPropertyNames(charMapA);
-  const bProps = Object.getOwnPropertyNames(charMapB);
-
-  if (aProps.length !== bProps.length) return false;
-
-  for (let x = 0; x < aProps.length; x += 1) {
-    if (charMapA[x] !== charMapB[x]) return false;
-  }
-
-  return true;
+  return areEqualObjects(charMapA, charMapB);
 }
 
 module.exports = isAnagram;
