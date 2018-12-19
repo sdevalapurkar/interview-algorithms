3f1483ab0d16152ba24e29e2465fc0c3dd842bb9
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-18 21:38:25 -0800

    add pyramid solution :)

--- pyramid/pyramid.js
+++ pyramid/pyramid.js
@@ -1,7 +1,26 @@
 // write a function that accepts a number n and outputs n levels of a pyramid shape
 
 function generatePyramid(n) {
+  for (let x = n; x > 0; x -= 1) {
+    let counter = 0;
+    let pyramidString = '';
+    const pyramidLevel = n - x + 1;
 
+    while (counter < (pyramidLevel * 2) - 1) {
+      pyramidString += '#';
+      counter += 1;
+    }
+
+    let numSpaces = x - 1;
+
+    while (numSpaces > 0) {
+      pyramidString = ` ${pyramidString} `;
+      numSpaces -= 1;
+    }
+
+    /* eslint-disable no-console */
+    console.log(pyramidString);
+  }
 }
 
 module.exports = generatePyramid;
--- pyramid/pyramid.test.js
+++ pyramid/pyramid.test.js
@@ -9,19 +9,27 @@ afterEach(() => {
   console.log.mockRestore();
 });
 
-test('generateSteps function is defined', () => {
+test('generatePyramid function is defined', () => {
   expect(generatePyramid).toBeDefined();
 });
 
-test('outputs the right steps when called with n = 1', () => {
+test('outputs the right pyramid when called with n = 1', () => {
   generatePyramid(1);
   expect(console.log.mock.calls[0][0]).toEqual('#');
   expect(console.log.mock.calls.length).toEqual(1);
 });
 
-test('outputs the right steps when called with n = 2', () => {
+test('outputs the right pyramid when called with n = 2', () => {
   generatePyramid(2);
   expect(console.log.mock.calls[0][0]).toEqual(' # ');
   expect(console.log.mock.calls[1][0]).toEqual('###');
   expect(console.log.mock.calls.length).toEqual(2);
+});
+
+test('outputs the right pyramid when called with n = 3', () => {
+  generatePyramid(3);
+  expect(console.log.mock.calls[0][0]).toEqual('  #  ');
+  expect(console.log.mock.calls[1][0]).toEqual(' ### ');
+  expect(console.log.mock.calls[2][0]).toEqual('#####');
+  expect(console.log.mock.calls.length).toEqual(3);
 });
