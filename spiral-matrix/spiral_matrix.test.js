bf4db558e8cf77763be7403dcb8f2d1ef179e516
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-21 18:51:44 -0800

    add more spiral tests

--- spiral-matrix/spiral_matrix.test.js
+++ spiral-matrix/spiral_matrix.test.js
@@ -4,7 +4,21 @@ test('generateSpiralMatrix function is defined', () => {
   expect(generateSpiralMatrix).toBeDefined();
 });
 
-test('returns the proper spiral nxn matrix', () => {
+test('works when n = 0', () => {
+  const spiralMatrix = generateSpiralMatrix(0);
+
+  expect(spiralMatrix).toEqual(expect.arrayContaining([]));
+});
+
+test('works for n = 1', () => {
+  const spiralMatrix = generateSpiralMatrix(1);
+
+  expect(spiralMatrix).toEqual(expect.arrayContaining([
+    [1],
+  ]));
+});
+
+test('returns the proper spiral 3x3 matrix', () => {
   const spiralMatrix = generateSpiralMatrix(3);
 
   expect(spiralMatrix).toEqual(expect.arrayContaining([
@@ -12,10 +26,24 @@ test('returns the proper spiral nxn matrix', () => {
   ]));
 });
 
-test('returns the proper spiral nxn matrix', () => {
+test('returns the proper spiral 4x4 matrix', () => {
   const spiralMatrix = generateSpiralMatrix(4);
 
   expect(spiralMatrix).toEqual(expect.arrayContaining([
     [1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7],
+  ]));
+});
+
+test('returns the proper spiral 7x7 matrix', () => {
+  const spiralMatrix = generateSpiralMatrix(7);
+
+  expect(spiralMatrix).toEqual(expect.arrayContaining([
+    [1, 2, 3, 4, 5, 6, 7],
+    [24, 25, 26, 27, 28, 29, 8],
+    [23, 40, 41, 42, 43, 30, 9],
+    [22, 39, 48, 49, 44, 31, 10],
+    [21, 38, 47, 46, 45, 32, 11],
+    [20, 37, 36, 35, 34, 33, 12],
+    [19, 18, 17, 16, 15, 14, 13],
   ]));
 });
