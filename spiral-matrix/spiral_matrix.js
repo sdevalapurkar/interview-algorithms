5b84fd661b7664895b4b8939af1f79546df2ae4c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-21 18:41:52 -0800

    add spiral matrix solution

--- spiral-matrix/spiral_matrix.js
+++ spiral-matrix/spiral_matrix.js
@@ -1,7 +1,49 @@
 // write a function that accepts an integer n and outputs a nxn spiral matrix
 
 function generateSpiralMatrix(n) {
+  const spiralMatrix = [];
 
+  for (let x = 0; x < n; x += 1) {
+    spiralMatrix.push([]);
+  }
+
+  let counter = 1;
+  let startRow = 1;
+  let endRow = n;
+  let startCol = 1;
+  let endCol = n;
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let x = startCol; x <= endCol; x += 1) {
+      spiralMatrix[startCol - 1][x - 1] = counter;
+      counter += 1;
+    }
+
+    startRow += 1;
+
+    for (let x = startRow; x <= endRow; x += 1) {
+      spiralMatrix[x - 1][endCol - 1] = counter;
+      counter += 1;
+    }
+
+    endCol -= 1;
+
+    for (let x = endCol; x >= startCol; x -= 1) {
+      spiralMatrix[endRow - 1][x - 1] = counter;
+      counter += 1;
+    }
+
+    endRow -= 1;
+
+    for (let x = endRow; x >= startRow; x -= 1) {
+      spiralMatrix[x - 1][startCol - 1] = counter;
+      counter += 1;
+    }
+
+    startCol += 1;
+  }
+
+  return spiralMatrix;
 }
 
 module.exports = generateSpiralMatrix;
--- spiral-matrix/spiral_matrix.test.js
+++ spiral-matrix/spiral_matrix.test.js
@@ -11,3 +11,11 @@ test('returns the proper spiral nxn matrix', () => {
     [1, 2, 3], [8, 9, 4], [7, 6, 5],
   ]));
 });
+
+test('returns the proper spiral nxn matrix', () => {
+  const spiralMatrix = generateSpiralMatrix(4);
+
+  expect(spiralMatrix).toEqual(expect.arrayContaining([
+    [1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7],
+  ]));
+});
