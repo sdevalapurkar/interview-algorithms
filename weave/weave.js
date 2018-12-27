f55e49213de028e06defd18cc4c14db21c165e8d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-26 20:55:33 -0800

    simplify solution

--- weave/weave.js
+++ weave/weave.js
@@ -4,21 +4,14 @@ const Queue = require('../queue/queue');
 
 function weave(queue1, queue2) {
   const outputQueue = new Queue();
-  let isFirstQueue = true;
 
-  while (queue1.peek() !== null || queue2.peek() !== null) {
-    if (isFirstQueue) {
-      if (queue1.peek() !== null) {
-        outputQueue.add(queue1.remove());
-        isFirstQueue = false;
-      } else {
-        isFirstQueue = false;
-      }
-    } else if (queue2.peek() !== null) {
+  while (queue1.peek() || queue2.peek()) {
+    if (queue1.peek()) {
+      outputQueue.add(queue1.remove());
+    }
+
+    if (queue2.peek()) {
       outputQueue.add(queue2.remove());
-      isFirstQueue = true;
-    } else {
-      isFirstQueue = true;
     }
   }
 
