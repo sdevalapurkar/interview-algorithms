4f345c7b05f517e0e4eb04f696d42f8aa2831210
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-12-28 18:20:57 -0800

    add node class and constructor for linkedlist class

--- linked-list/linked_list.js
+++ linked-list/linked_list.js
@@ -1,4 +1,17 @@
 // implement the node class and linked list class
 // based on the directions specified in specifications.html
 
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
 module.exports = { Node, LinkedList };
